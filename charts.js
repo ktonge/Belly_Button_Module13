d1d0eaf2a63c4a9901aae7366ee1a50daeaf6c5c
ktonge <karltonge9@gmail.com>
2023-01-26 23:32:20 -0500

    Revert "Update charts.js"
    This reverts commit d32d40e96d041fc5fd60fce24abab989382b47a8.

--- charts.js
+++ charts.js
@@ -119,8 +119,6 @@ function buildCharts(sample) {
     var gaugeData = [{
       domain: {x: [0,1],y: [0,1]},
       value: resultsWfreq,
-      type: "indicator",
-		  mode: "gauge+number"
       gauge: {
         axis: {range: [0,10]},
         steps: [
